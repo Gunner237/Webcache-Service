83f4e8700435b5bae5c53617808d29e323701fab
Sebastian Gunner <Gunner237@users.noreply.github.com>
2019-01-22 22:19:57 +0000

    Update request.php
    ~ Use CURL rather than file_get_contents
    ~ Restricted to JSON files only

--- web/request.php
+++ web/request.php
@@ -1,15 +1,9 @@
 <?php
-    // Add user agent to comply with GitHub's documentation
-    $opts = [
-    'http' => [
-    'method' => 'GET',
-    'header' => [
-    'User-Agent: Gunner237'
-    ]
-    ]
-    ];
     
-    $context = stream_context_create($opts);
+    function isJson($string) {
+        json_decode($string);
+        return (json_last_error() == JSON_ERROR_NONE);
+    }
     
     $url = $_GET['u'];
     
@@ -18,8 +12,22 @@
     
     // Validate url
     if (!filter_var($url, FILTER_VALIDATE_URL) === false) {
-        $jsonData = file_get_contents($url,false,$context);
-        echo $jsonData;
+        //$jsonData = file_get_contents($url,false,$context);
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch,CURLOPT_USERAGENT,'Gunner237.WebCacheService');
+        $jsonData = curl_exec($ch);
+        curl_close($ch);
+        
+        
+        if (isJson($jsonData)){
+            echo $jsonData;
+        }
+        else{
+            echo("$url does not contain JSON data, so the webcaching service was unable to retrieve it successfully");
+        }
     }
     else {
         echo("$url is not a valid URL");
